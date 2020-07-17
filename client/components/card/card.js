f5bfd5b0a14aabcd93e6379b733da92ac8254603
Tnk2U <rndskadl@gmail.com>
2020-07-17 09:46:56 +0900

    feat: column컴포넌트, card컴포넌트 추가 ?: column, card컴포넌트를 추가하고 client/index.js에서 호출하여 화면 렌더링시 초기화면을 띄워주도록 구현

---
+++ client/components/card/card.js
@@ -0,0 +1,23 @@
+export default class Card {
+  constructor(parentDom, data) {
+    this.parentDom = parentDom;
+    this.data = data;
+    this.render();
+  }
+
+  render() {
+    this.parentDom.innerHTML += `<div class="card_wrap">
+        <div class="card_content">
+            <img class="img_card" src="https://static.thenounproject.com/png/14910-200.png"/>
+            <div class="card_note">
+                <span>${this.data.note}</span>
+            </div>
+            <button class="btn_delete">x</button>
+        </div>
+        <div class="card_bottom">
+            <span class="add_by">Added by</span>
+            <span class="span_writer">${this.data.writer}</span>
+        </div>
+      <div>`;
+  }
+}
