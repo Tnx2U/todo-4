5500202940bd58eeabaa9a89395f84f4864a83ac
yuda1124 <yuda1124@naver.com>
2020-07-22 20:55:25 +0900

    feat: captured card 구현
    ?: card 드래그 시 captured card visible, 드래그 끝날 시 hidden

--- client/controllers/dragAndDrop.js
+++ client/controllers/dragAndDrop.js
@@ -1,22 +1,61 @@
 export default class DragAndDrop {
   static columnRootElement = null;
+  static draggedCard = null;
+  static enteredColumn = null;
   static initialize() {
     this.columnRootElement = document.querySelector(".column_wrap");
     this.columnRootElement.addEventListener("mousedown", this.onMouseDown);
   }
 
   static onMouseDown = (e) => {
+    this.setDraggedCard(e);
+    if (!this.isCardDragged()) return;
+    this.setCapturedCard();
+    this.setRelativePositionInCard(e);
     this.columnRootElement.addEventListener("mousemove", this.onMouseMove);
     this.columnRootElement.addEventListener("mouseup", this.onMouseUp);
   };
 
+  static setDraggedCard(e) {
+    const draggedCard = e.target.closest(".card_wrap");
+    if (draggedCard) {
+      this.draggedCard = draggedCard;
+    }
+  }
+
+  static clearDraggedCard() {
+    this.draggedCard = null;
+  }
+
+  static isCardDragged() {
+    return this.draggedCard !== null;
+  }
+
   static onMouseMove = (e) => {
-    // document.querySelector(".ondrag").style.left = e.offsetX + "px";
-    // document.querySelector(".ondrag").style.top = e.offsetY + "px";
+    this.capturedCard.style.left =
+      e.clientX - 17 - this.relativeLeftInCard + "px";
+    this.capturedCard.style.top =
+      e.clientY - 17 - this.relativeTopInCard + "px";
   };
 
   static onMouseUp = (e) => {
     this.columnRootElement.removeEventListener("mousemove", this.onMouseMove);
     this.columnRootElement.removeEventListener("mouseup", this.onMouseUp);
+    this.capturedCard.classList.add("hidden");
+    this.clearDraggedCard();
   };
+
+  static setCapturedCard() {
+    this.capturedCard = document.querySelector("#card_capture");
+    this.capturedCard.innerHTML = this.draggedCard.innerHTML;
+    this.capturedCard.classList.remove("hidden");
+  }
+
+  static setRelativePositionInCard(e) {
+    const bounds = this.draggedCard.getBoundingClientRect();
+    this.relativeLeftInCard = e.clientX - bounds.left;
+    this.relativeTopInCard = e.clientY - bounds.top;
+    this.capturedCard.style.left = bounds.left - 17 + "px";
+    this.capturedCard.style.top = bounds.top - 17 + "px";
+  }
 }
